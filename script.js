b0a9392528312f54a847002bb58447c19c1ecd1f
zire98 <164895719+zire98@users.noreply.github.com>
2025-07-03 14:12:05 +0200

    Operator functions.

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function substract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
