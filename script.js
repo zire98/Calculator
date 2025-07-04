b802f1fc6f4a9e48de9e0caf2000410841087c2c
zire98 <164895719+zire98@users.noreply.github.com>
2025-07-04 15:59:18 +0200

    Working on the calculator logic
    It includes a function to obtain the button type, a function to perform the required operation that receives both numbers and the operator as input parameters, and a function to carry out all the calculator logic, thus leaving the code clean and scalable, and only calling these functions in the event. It also includes a function to display what is needed on the calculator screen.

--- script.js
+++ script.js
@@ -14,3 +14,94 @@ function divide(number1, number2) {
     return number1 / number2;
 }
 
+function typeButton(e) {
+    if (e.target.classList.contains("number")) {
+        return "number";
+    } else if (e.target.classList.contains("operator")) {
+        return operator;
+    } else if (e.target.id == "result") {
+        return "result";
+    } else if (e.target.id == "decimal") {
+        return "decimal";
+    }
+}
+
+function check(value) {
+    value == undefined ? true : false;
+}
+
+function result(firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+            break;
+
+        case "-":
+            return substract(firstNumber, secondNumber);
+            break;
+
+        case "*":
+            return multiply(firstNumber, secondNumber);
+            break;
+
+        case "/":
+            return divide(firstNumber, secondNumber);
+            break;
+    }
+}
+
+function logic(button, typeButton, firstNumber, operator, secondNumber) {
+    switch (typeButton) {
+        case "number":
+            if (operator === undefined) {
+                firstNumber += button.textContent;
+                show(firstNumber);
+            } else {
+                secondNumber += button.textContent;
+                show(secondNumber);
+            }
+            break;
+
+        case "operator":
+            operator = button.textContent;
+            show(operator);
+            break;
+
+        case "decimal":
+            if (operator === undefined) {
+                firstNumber.contains(".") ? false : firstNumber += button.textContent;
+                show(firstNumber);
+
+            } else {
+                secondNumber.contains(".") ? false : firstNumber += button.textContent;
+                show(secondNumber);
+            }
+            break;
+
+        case "result":
+            firstNumber == result(firstNumber, operator, secondNumber);
+            show(firstNumber);
+            operator = "";
+            secondNumber = "";
+            break;
+    }
+}
+
+function show(result) {
+    let screen = document.querySelector("#screen");
+    screen.textContent = result;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let buttons = document.querySelectorAll('button');
+    let firstNumber;
+    let operator;
+    let secondNumber;
+
+    buttons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            let typeButton = typeButton(e);
+            logic(button, typeButton, firstNumber, operator, secondNumber);
+        })
+    });
+})
